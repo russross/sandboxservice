8463981a189d3759f5d543fba55c1d11df592f59
Russ Ross <russ@dixie.edu>
2012-11-09 18:05:18 -0700

    working for unsandboxed inputoutput

--- python27.go
+++ python27.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
 )
+
+const python27name = "bin/python2.7-static"
+
+var python27path string
+
+func init() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic("Failed to find working directory: " + err.Error())
+	}
+	python27path = filepath.Join(wd, python27name)
+}
 
 type InputOutputTest string
 
@@ -40,7 +59,7 @@ type Request struct {
 	Reference string
 	Candidate string
 
-	Tests []*InputOutputTest
+	Tests []InputOutputTest
 }
 
 func (elt *Request) Validate() error {
@@ -82,6 +101,71 @@ func (elt *Request) Validate() error {
 	return nil
 }
 
+func (req *Request) RunTest(test InputOutputTest, ref *TestResult) (*InputOutputTestResult, error) {
+	// create a sandbox directory
+	dirname, err := ioutil.TempDir("", "sandbox")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create working directory: %v", err)
+	}
+	defer os.RemoveAll(dirname)
+
+	// set up the environment files
+	stdin := strings.NewReader(string(test))
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	source := req.Candidate
+	if ref == nil {
+		source = req.Reference
+	}
+	if err := ioutil.WriteFile(filepath.Join(dirname, "source.py"), []byte(source), 0644); err != nil {
+		return nil, fmt.Errorf("Failed to create source.py file: %v", err)
+	}
+
+	// execute the test
+	start := time.Now()
+	cmd := exec.Command(python27path, "source.py")
+	cmd.Dir = dirname
+	cmd.Stdin = stdin
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	err = cmd.Run()
+	finish := time.Now()
+	elapsed := finish.Sub(start).Seconds()
+
+	success := err == nil && cmd.ProcessState.Success()
+	exit := 0
+	if !success {
+		exit = -1
+	}
+	testres := &TestResult{
+		Killed:     !success,
+		ExitStatus: exit,
+		Message:    cmd.ProcessState.String(),
+		Stdout:     stdout.String(),
+		Return:     "",
+		Stderr:     stderr.String(),
+	}
+
+	result := &InputOutputTestResult{
+		Success: success,
+
+		CPUSeconds:   (cmd.ProcessState.SystemTime() + cmd.ProcessState.UserTime()).Seconds(),
+		TotalSeconds: elapsed,
+		MB:           0.0,
+
+		Reference: ref,
+		Candidate: testres,
+	}
+
+	if ref == nil {
+		result.Reference = testres
+		result.Candidate = nil
+	}
+
+	return result, nil
+}
+
 func grade_python27_inputoutput(w http.ResponseWriter, r *http.Request, decoder *json.Decoder) {
 	in := new(Request)
 	if err := decoder.Decode(in); err != nil {
@@ -93,30 +177,23 @@ func grade_python27_inputoutput(w http.ResponseWriter, r *http.Request, decoder 
 		return
 	}
 
-	result := &InputOutputTestResult{
-		Success: true,
+	results := []*InputOutputTestResult{}
 
-		CPUSeconds:   0.73,
-		TotalSeconds: 0.74,
-		MB:           8.125,
-
-		Reference: &TestResult{
-			Killed:     false,
-			ExitStatus: 0,
-			Message:    "",
-			Stdout:     "Hello, world\n",
-			Return:     "",
-			Stderr:     "",
-		},
-
-		Candidate: &TestResult{
-			Killed:     false,
-			ExitStatus: 0,
-			Message:    "",
-			Stdout:     "Hello, world\n",
-			Return:     "",
-			Stderr:     "",
-		},
+	for n, test := range in.Tests {
+		// run it once with the reference solution
+		ref, err := in.RunTest(test, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error running reference solution %d: %v", n, err), http.StatusInternalServerError)
+			return
+		}
+		cand, err := in.RunTest(test, ref.Reference)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error running candidate solution %d: %v", n, err), http.StatusInternalServerError)
+			return
+		}
+		cand.Success = ref.Success && cand.Success && cand.Candidate.Stdout == cand.Reference.Stdout
+		results = append(results, cand)
 	}
-	writeJson(w, r, result)
+
+	writeJson(w, r, results)
 }
